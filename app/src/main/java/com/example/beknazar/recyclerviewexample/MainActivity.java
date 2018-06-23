1dfdfe175a19c16cdcfb6b72d8730c4d718f6d20
Beknazar <beknazark@inbox.ru>
2018-06-23 15:13:30 +0600

    Initial commit

---
+++ app/src/main/java/com/example/beknazar/recyclerviewexample/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.beknazar.recyclerviewexample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView  recyclerView;
+    private LinearLayoutManager verticalLinearLayoutManager;
+    private LinearLayoutManager horizontalLinearLayoutManager;
+    private RecyclerAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recyclerView = (RecyclerView) findViewById(R.id.recycler);
+
+        verticalLinearLayoutManager = new LinearLayoutManager(this);
+        horizontalLinearLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(verticalLinearLayoutManager);
+        adapter= new RecyclerAdapter();
+        recyclerView.setAdapter(adapter);
+        adapter.addAll(ModulItem.getFakeItems);
+
+    }
+}
---
+++ app/src/main/java/com/example/beknazar/recyclerviewexample/ModulItem.java
@@ -0,0 +1,18 @@
+package com.example.beknazar.recyclerviewexample;
+
+import java.security.PublicKey;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ModulItem {
+    private String author;
+    private  int imgId;
+
+ public ModulItem(String author, int imgId){
+     this.author=author;
+     this.
+
+
+ }
+
+}
